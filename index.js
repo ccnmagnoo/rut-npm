461b5cb73acfa3419f013a4643190fc299e93555
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-04-27 10:11:29 -0400

    fix function name

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isRut = void 0;
+exports.rolCV = exports.isRut = void 0;
+const rolChecker_1 = require("./rolChecker");
+const rolDigitGe_1 = require("./rolDigitGe");
 function isRut(rol) {
     //social number verification
-    const rolChecker = (rol) => {
-        //undefinded solution
-        if (rol === undefined)
-            return { result: false, error: 'fake number' };
-        rol = rol === null || rol === void 0 ? void 0 : rol.replace('‐', '-');
-        rol = rol === null || rol === void 0 ? void 0 : rol.split('.').join('');
-        const re = /^[0-9]+[-|‐]{1}[0-9kK]{1}$/;
-        if (rol === '11111111-1')
-            return { result: false, error: 'fake number' };
-        if (!re.test(rol)) {
-            return { result: false, error: 'wrong id' };
-        }
-        else {
-            const rolSplited = rol.split('-');
-            let rolDigit = rolSplited[1];
-            const rolBody = rolSplited[0];
-            if (rolDigit === 'K') {
-                rolDigit = 'k';
-            }
-            //validation
-            return rolDigitGen(+rolBody) === rolDigit
-                ? { result: true, error: undefined }
-                : { result: false, error: 'wrong last digit' };
-        }
-    };
-    const rolDigitGen = (rol) => {
-        //calculated last digit
-        let M = 0, S = 1;
-        for (; rol; rol = Math.floor(rol / 10)) {
-            S = (S + (rol % 10) * (9 - (M++ % 6))) % 11;
-        }
-        return S ? (S - 1).toString() : 'k';
-    };
-    return rolChecker(rol);
+    return (0, rolChecker_1.rolChecker)(rol);
 }
 exports.isRut = isRut;
+exports.rolCV = rolDigitGe_1.rolDigitGen;
